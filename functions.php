2a09dc28b2eccc9f15524849a4d6582ca4abd786
baileyfcross <bf02cros@siena.edu>
2022-04-19 20:51:57 -0400

    added new php index

---
+++ functions.php
@@ -0,0 +1,37 @@
+<?php
+
+function header()
+{
+
+}
+
+function footer(){
+  echo '
+  <div id="footer">
+  <div class="container">
+
+    <div class ="column">
+      <h4>Contact Me</h4>
+      <p>
+      <a href="https://github.com/baileyfcross">GitHub</a>
+      <br>
+      <a href="https://www.linkedin.com/in/bailey-cross-a636b7183/">LinkedIn</a>
+      </p>
+    </div>
+
+    <div class="column">
+    <h4>My Story</h4>
+    <p>Hello, I\'m Bailey Cross, a current Software Development student at Siena College.</p>
+    </div>
+
+  </div>
+  </div>
+  ';
+}
+
+
+
+
+
+
+ ?>
